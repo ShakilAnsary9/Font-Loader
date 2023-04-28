d3510fc6a66548ee9293f088304f83d126b75663
John Doe <shakilansary9@gmail.com>
2023-04-28 12:05:57 +0530

    src update

---
+++ script.js
@@ -0,0 +1,6 @@
+var link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute("href", "path/to/your/css/file.css");
+link.setAttribute("crossorigin", "anonymous");
+document.head.appendChild(link);
